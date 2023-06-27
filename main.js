a054fbb14626bff09bf2b99942a9e0cb1f0f8e9b
ISAIAS <issuyep@yahoo.com>
2023-06-26 20:39:24 -0600

    creating dropdown function

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
-console.log('connected')
-console.log('perfect')
+//https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+// Elements
+const cocktailSelect = document.querySelector("#cocktails");
+const cataegorySelect = document.querySelector("#categories");
+//Function calls
+getCocktails();
+getCategories();
+
+// Functions
+function getCocktails() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data);
+        const cocktails = data.drinks;
+        cocktails.forEach((cocktail) => {
+            const option = document.createElement("option");
+            option.value = cocktail.idDrink;
+            option.innerText = cocktail.strDrink;
+            cocktailSelect.appendChild(option);
+        });
+        });
+    }
+
+function getCategories() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data);
+        const categories = data.drinks;
+        categories.forEach((category) => {
+            const option = document.createElement("option");
+            option.value = category.strCategory;
+            option.innerText = category.strCategory;
+            cataegorySelect.appendChild(option);
+        });
+        });
+    }
+
+
+function renderCocktailOptions(cocktail) {
+    const option = document.createElement("option");
+    option.value = cocktail.idDrink;
+    option.textContent = cocktail.strDrink;
+    cocktailSelect.appendChild(option);
+}
+
+function renderCategoryOptions(category) {
+    const option = document.createElement("option");
+    option.value = category.strCategory;
+    option.textContent = category.strCategory;
+    cataegorySelect.appendChild(option);
+}
+
---
+++ scratch.md
@@ -0,0 +1,29 @@
+# phase 1 project notes
+
+## Links
+
+- assignment link - https://my.learn.co/courses/649/assignments/32803?module_item_id=82993
+- guidelines link - https://my.learn.co/courses/649/pages/phase-1-project-guidelines?module_item_id=82990
+
+## TODO List
+
+
+- [x] - call public API
+
+## Requirements
+
+### Obligatory
+
+- html ,css ,js
+- public API or db.json
+- 2 distinct event listeners
+- iterating through arrays
+- need to use array methods
+- single page only
+- 30+ commits
+- render data in the HTML DOM
+- array, 5 objects, 4 key-value pairs
+
+### Extras
+
+- Mobile Friendly
