5bda7a30a0dfd3e0543549ae6dbbbd8455ad5864
ISAIAS <issuyep@yahoo.com>
2023-06-30 16:22:32 -0600

    gets recipes by cocktails

--- main.js
+++ main.js
@@ -3,53 +3,76 @@
 // Elements
 const cocktailSelect = document.querySelector("#cocktails");
 const cataegorySelect = document.querySelector("#categories");
+const recipeContainer = document.querySelector(".recipe-container");
 //Function calls
 getCocktails();
 getCategories();
+// Event Listeners
+cocktailSelect.addEventListener("change",getRecipeByCocktail);
+cataegorySelect.addEventListener("change",getRecipeByCocktail);
 
-// Functions
+    // Functions
 function getCocktails() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
         .then((response) => response.json())
-        .then((data) => {
-        console.log(data);
-        const cocktails = data.drinks;
-        cocktails.forEach((cocktail) => {
-            const option = document.createElement("option");
-            option.value = cocktail.idDrink;
-            option.innerText = cocktail.strDrink;
-            cocktailSelect.appendChild(option);
-        });
-        });
-    }
+        .then(cocktails => renderCocktailOptions(cocktails))
+        .catch(error =>alert(error))
+}
 
 function getCategories() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+
         .then((response) => response.json())
-        .then((data) => {
-        console.log(data);
-        const categories = data.drinks;
-        categories.forEach((category) => {
-            const option = document.createElement("option");
-            option.value = category.strCategory;
-            option.innerText = category.strCategory;
-            cataegorySelect.appendChild(option);
-        });
-        });
-    }
-
-
-function renderCocktailOptions(cocktail) {
+        .then(Categories => renderCategoryOptions(Categories))
+        .catch(error =>alert(error))
+}
+function renderCocktailOptions(Cocktails) {
+    Cocktails.drinks.forEach(cocktail => {
     const option = document.createElement("option");
     option.value = cocktail.idDrink;
     option.textContent = cocktail.strDrink;
     cocktailSelect.appendChild(option);
+    });
 }
 
-function renderCategoryOptions(category) {
+function renderCategoryOptions(Categories) {
+    Categories.drinks.forEach(category => {
     const option = document.createElement("option");
     option.value = category.strCategory;
     option.textContent = category.strCategory;
     cataegorySelect.appendChild(option);
+    });
 }
 
+//Recipe collection functions
+function getRecipeByCocktail(e) {
+    const cocktail = e.target.value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail}`)
+        .then((response) => response.json())
+        .then(recipes=> renderAllRecipes(recipes.drinks))
+        .catch(error =>alert(error))
+}
+
+function renderAllRecipes(recipes) {
+    recipes.forEach(recipe => {
+        renderRecipeCard(recipe);
+    });
+    cocktailSelect.value = "";
+    cataegorySelect.value = "";
+}
+
+function renderRecipeCard(recipe) {
+    const { strDrink, strDrinkThumb, strInstructions} = recipe;
+console.log(recipe);
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const img = document.createElement("img");
+    img.src = strDrinkThumb;
+
+    cardDiv.append(img);
+    recipeContainer.append(cardDiv);
+}
+
+    
